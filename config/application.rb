d5c14e5dbc8cac3e9af1f3afe039bd69f411ca13
guilherme <developersposito@gmail.com>
2020-03-04 16:54:13 -0300

    Configure db, and add gems Create local_env and and variable for db; Add Gems Add Trevis CI.

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,12 @@ module PortalSolarDevTest
   class Application < Rails::Application
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    config.before_configuration do
+      env_file = File.join(Rails.root + 'config/local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exist?(env_file)
+    end
   end
 end
